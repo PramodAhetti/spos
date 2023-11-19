7420632a49608ee5a025e3f4f7ad73c5bc1e9735
PramodAhetti <pramodahetti05@gmail.com>
2023-11-19 23:44:02 +0530

    sjf correct preemptive code

--- cpuSchedule/sjf.java
+++ cpuSchedule/sjf.java
@@ -4,35 +4,69 @@ import java.io.*;
 import java.util.*;
 
 class sjf {
+
+    public static int least(int completeTime,int no,int arrival[],int burst[],int flag[]){
+            int minIndex=-1;
+            int minValue=1000;
+            for(int i=0;i<no;i++){
+                if(flag[i]==1 && burst[i]<=minValue && arrival[i]<=completeTime){
+                  minIndex=i;
+                  minValue=burst[i];
+                }
+            }
+
+            return minIndex;
+    }
 	public static void main (String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("enter no of process:");
         int no=s.nextInt();
-        Integer process[]=new Integer[no];
+        int arrival[]=new int[no];
+        int flag[]=new int[no];
         int burst[]=new int[no];
+        int ogburst[]=new int[no];
+        int cTime[]=new int[no];
+        int taTime[]=new int[no];
+        int waiting[]=new int[no];
+        int visited[]=new int[no];
         for(int i=0;i<no;i++){
-            process[i]=i+1;
+            System.out.println("Enter arrival time for "+(i+1)+":");
+            arrival[i]=s.nextInt();
             System.out.println("Enter burst time for "+(i+1)+":");
-            burst[i]=s.nextInt();
+            burst[i]=s.nextInt(); 
+            ogburst[i]=burst[i]; 
+            waiting[i]=0;
+            visited[i]=-1; 
         }
-        for(int i=0;i<no;i++){
-            for(int j=i;j<no;j++){
-                if(burst[i]>burst[j]){
-                   int temp=process[i];
-                   process[i]=process[j];
-                   process[j]=temp;
-                   temp=burst[i];
-                   burst[i]=burst[j];
-                   burst[j]=temp;
-                }
-            }
-        }
+        Arrays.fill(flag,1);
         int completeTime=0;
-        System.out.println("Id Bt Wt Tat");
-        for(int i=0;i<no;i++){
-            completeTime+=burst[i];
-            System.out.println(process[i]+"  "+burst[i]+"  "+(completeTime-burst[i])+" "+(completeTime));
+        int count=0;
+        while(count<no){
+             
+             int temp=least(completeTime,no,arrival,ogburst,flag);
+             System.out.println(temp+" "+completeTime);
+             burst[temp]--;
+             if(visited[temp]==-1){
+                visited[temp]=completeTime;
+                waiting[temp]+=completeTime-arrival[temp];
+             }else{
+             waiting[temp]+=(completeTime-visited[temp]-1);
+             visited[temp]=completeTime;
+             }
+             completeTime++;
+             if(burst[temp]<=0){
+                System.out.println("here : "+temp+" "+completeTime);
+                count++;
+                flag[temp]=0;
+                cTime[temp]=completeTime;
+                taTime[temp]=cTime[temp]-arrival[temp];
+             }
+             
+             
         }
-
+      System.out.println("AT  BT  CT  TAT");
+      for(int i=0;i<no;i++){
+        System.out.println(arrival[i]+"   "+ogburst[i]+"   "+cTime[i]+"     "+taTime[i]+"    "+waiting[i]);
+      }
 	}
 }
